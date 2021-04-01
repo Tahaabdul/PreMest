d407dd2da37038572b4e3504e5514d681001ab58
Tahaabdul <rhaxaqi@gmail.com>
2021-04-01 07:38:18 +0000

    -

--- blogtest/src/index.js
+++ blogtest/src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< Updated upstream:blogtest/src/index.js
-=======
 
->>>>>>> Stashed changes:whethrapp/src/index.js
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,8 +14,7 @@ ReactDOM.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-<<<<<<< Updated upstream:blogtest/src/index.js
+
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-=======
+
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitalsreportWebVitals();
->>>>>>> Stashed changes:whethrapp/src/index.js
