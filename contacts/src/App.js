ca79772c0ddeebfcfde15afbd59ce780fd83e086
Tahaabdul <rhaxaqi@gmail.com>
2021-05-27 11:11:59 +0000

    contacts app created

---
+++ contacts/src/App.js
@@ -0,0 +1,14 @@
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
